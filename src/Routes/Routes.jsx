cd0732ffb061a040d065db2808a8a436bbb49211
lcorn26 <lcorn268@mtroyal.ca>
2021-11-09 03:07:26 -0700

    Fixed Web functionality

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -9,16 +9,19 @@ import { PlaysList } from "../Pages/PlayList";
 import { Favourites } from "../Pages/Favourites";
 import { PlayDetails } from "../Pages/PlayDetails";
 
+
+
+
 export function Routings() {
     return (
         <div>
-            <Router>
+            <Router basename={window.location.pathname || ''}>
                 <Routes>
-                    <Route path='/' element={<Home/>}></Route>
-                    <Route path='/play-list' element={<PlaysList/>}></Route>
-                    <Route path='/play-details/:id' element={<PlayDetails/>}></Route>
-                    <Route path='/favourites' element={<Favourites/>}></Route>
-                    <Route path="*" element={<Empty/>}/>
+                    <Route exact path='/' component={Home} element={<Home/>}></Route>
+                    <Route exact path='/play-list' component={PlaysList} element={<PlaysList/>}></Route>
+                    <Route exact path='/play-details/:id' component={PlayDetails} element={<PlayDetails/>}></Route>
+                    <Route exact path='/favourites' component={Favourites} element={<Favourites/>}></Route>
+                    <Route exact path="*" component={Empty} element={<Empty/>}/>
                 </Routes>
             </Router>
         </div>
